d98f7c6d2bffd4763903cc82e7a462244314bb02
arnav-iyer <119526190+arnav-iyer@users.noreply.github.com>
2023-11-03 10:19:23 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Image from 'next/image'
+import map from '../public/map.svg'
+import MyV0Component from './myV0Component'
+import TestComponent from './testComponent'
+
+// paste your v0 components anywhere below!
+export default function Index({
+}) {
+  return (
+    <MyV0Component />
+  )
+}
---
+++ pages/myV0Component.js
@@ -0,0 +1,13 @@
+//you can overwrite this entire file with your v0 component, this is just a placeholder.
+
+import React from 'react';
+
+function TestComponent() {
+    return (
+    <div class="text-base">
+        <h1>I'm a placeholder for your v0 Component.  Go to pages/myV0Component.js in github to get started!</h1>
+    </div>
+    )
+}
+
+export default TestComponent;
